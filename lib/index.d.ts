08fac71ad8d3c01d9a30aa26d09f9af95751c64c
gustave anthony <anthony@gustave-auto.com>
2023-11-21 16:36:02 +0100

    feat: Update SupabaseExtraClient class and createExtraClient function
    - Updated the SupabaseExtraClient class to use the openapi-client-axios library for making API requests.
    - Modified the createExtraClient function to accept an additional parameter, apiOptions, which includes the schema and URL for the API.
    - Replaced the deprecated axios library with openapi-client-axios in both index.d.ts and index.js files.
    - Removed the types.ts file as it is no longer needed.
    
    This commit improves the code by integrating openapi-client-axios for better handling of API requests.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,14 @@
-import { ICreateExtraClient } from "./types";
-export declare const createExtraClient: ICreateExtraClient;
+import { SupabaseClient, SupabaseClientOptions } from "@supabase/supabase-js";
+import { Document } from "openapi-client-axios";
+declare class SupabaseExtraClient<T> extends SupabaseClient {
+    backApi: T;
+    constructor(url: string, key: string, apiOptions: {
+        schema: any;
+        url: string;
+    }, options?: SupabaseClientOptions<"public">);
+}
+export declare const createExtraClient: <T>(url: string, key: string, apiOptions: {
+    schema: Document;
+    url: string;
+}, options?: SupabaseClientOptions<"public">) => SupabaseExtraClient<T>;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,51 @@
-import { SupabaseClient, SupabaseClientOptions } from "@supabase/supabase-js"
-import axios, { AxiosInstance } from "axios"
-import { ICreateExtraClient } from "./types"
+import { SupabaseClient, SupabaseClientOptions } from "@supabase/supabase-js";
+import OpenAPIClientAxios, { Document } from "openapi-client-axios";
 
-class SupabaseExtraClient extends SupabaseClient {
-  public backApi: AxiosInstance;
 
-  constructor(url: string, key: string, backApiUrl: string, options?: SupabaseClientOptions<"public">) {
-    super(url, key, options)
-    this.backApi = axios.create({
-      baseURL: backApiUrl,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + key,
-      },
-    })
+class SupabaseExtraClient<T> extends SupabaseClient {
+  public backApi: T;
+
+  constructor(
+    url: string,
+    key: string,
+    apiOptions: {
+      schema: any;
+      url: string;
+    },
+    options?: SupabaseClientOptions<"public">
+  ) {
+    super(url, key, options);
+    const openApiClient = new OpenAPIClientAxios({
+      definition: apiOptions.schema,
+      withServer: { url: apiOptions.url },
+      axiosConfigDefaults: {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + key
+        }
+      }
+    });
+    this.backApi = openApiClient.initSync<T>();
     this.auth.onAuthStateChange((event, session) => {
-      const token = session?.access_token || key 
-      this.backApi.defaults.headers.Authorization =
-        "Bearer " + token
-    })
+      const token = session?.access_token || key;
+      if (!openApiClient.axiosConfigDefaults.headers) {
+        openApiClient.axiosConfigDefaults.headers = {};
+      }
+      openApiClient.axiosConfigDefaults.headers.Authorization =
+        "Bearer " + token;
+      this.backApi = openApiClient.initSync<T>();
+    });
   }
 }
 
-export const createExtraClient: ICreateExtraClient = (
+export const createExtraClient = <T>(
   url: string,
   key: string,
   options: any,
-  backApiUrl: string,
+  apiOptions: {
+    schema: any;
+    url: string;
+  }
 ) => {
-  return new SupabaseExtraClient(url, key, backApiUrl, options)
-}
+  return new SupabaseExtraClient<T>(url, key, apiOptions, options);
+};
